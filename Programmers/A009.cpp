b40d9f9ae480531ea4951f4cfb0e2c46429ece2a
정다훈 <jdh@jeongdahun-ui-MacBookPro.local>
2025-01-08 14:32:57 +0900

    solve A009, A0010

---
+++ Programmers/A009.cpp
@@ -0,0 +1,28 @@
+/*
+Programmers
+<문자열 다루기 기본>
+문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요. 
+예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+
+제한사항
+s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+s는 영문 알파벳 대소문자 또는 0부터 9까지 숫자로 이루어져 있습니다.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool solution(string s) {
+    bool answer = false;
+    for(int i=0; i<s.length(); i++){
+        if((s[i]>='a' && s[i] <= 'z') || (s[i]>='A' && s[i] <= 'Z')) return false;
+    }
+    if(s.length() == 4 || s.length() == 6) answer = true;
+    return answer;
+}
+
+int main(){
+    cout << solution("4123");
+    return 0;
+}
