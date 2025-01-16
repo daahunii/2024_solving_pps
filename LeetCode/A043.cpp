fe9021fc1218a8cc4f6de61776aadabebcc6d4d2
정다훈 <jdh@jeongdahun-ui-MacBookPro.local>
2025-01-16 13:16:10 +0900

    solve A042 A043 A044

---
+++ LeetCode/A043.cpp
@@ -0,0 +1,60 @@
+/*
+Leet Code
+<14. Longest Common Prefix>
+
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+=================================================================================================
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+=================================================================================================
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int min = 201;
+        string str = "";
+        bool flag = true;
+        char tmp;
+        
+        for(int i = 0; i < strs.size(); i++){
+            if(strs[i].length() < min) min = strs[i].length();
+        }
+        for(int i = 0; i < min; i++){ 
+            tmp = strs[0][i];
+            for(int j = 0; j < strs.size(); j++){
+                if(tmp == strs[j][i]){
+                    if(j == (strs.size()-1))
+                    str += strs[j][i];  
+                    continue;
+                }
+                else{
+                    flag = false;
+                    break;
+                }  
+            }
+            if(!flag) break;
+        }
+        return str;
+    }
+};
+
+int main(){
+    vector<string> st;
+    st.push_back("flower");
+    st.push_back("flow");
+    st.push_back("flight");
+    cout << Solution().longestCommonPrefix(st);
+    return 0;
+}
