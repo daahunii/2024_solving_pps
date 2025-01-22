c72cb5dcf22834c245a3d2683493c28dc206b856
정다훈 <jdh@jeongdahun-ui-MacBookPro.local>
2025-01-22 12:55:57 +0900

    solve A081 A082 A083 A084 A085

---
+++ Baekjoon/A081.cpp
@@ -0,0 +1,42 @@
+/*
+Baekjoon
+<2693. N번째 큰 수>
+
+문제
+배열 A가 주어졌을 때, N번째 큰 값을 출력하는 프로그램을 작성하시오.
+
+배열 A의 크기는 항상 10이고, 자연수만 가지고 있다. N은 항상 3이다.
+===================================================================================
+입력
+첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 
+각 테스트 케이스는 한 줄로 이루어져 있고, 배열 A의 원소 10개가 공백으로 구분되어 주어진다. 
+이 원소는 1보다 크거나 같고, 1,000보다 작거나 같은 자연수이다.
+
+출력
+각 테스트 케이스에 대해 한 줄에 하나씩 배열 A에서 3번째 큰 값을 출력한다.
+===================================================================================
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int T, num;
+    vector<int> v;
+    vector<int> result;
+    cin >> T;
+    for(int i=0; i<T; i++){
+        for(int j=0; j<10; j++){
+            cin >> num;
+            v.push_back(num);
+        }
+        sort(v.begin(), v.end());
+        result.push_back(v[7]);
+        v.clear();
+    }
+    for(int i=0; i<result.size(); i++) cout << result[i] << endl;
+    return 0;
+}
